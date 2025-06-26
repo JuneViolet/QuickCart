0e8834d00ee46d4b72271110b654097b734d46b4
tieuhythang <tieuhythang2000@gmail.com>
2025-06-26 11:59:10 +0700

    save

--- app/order/return/ReturnHandler.jsx
+++ app/order/return/ReturnHandler.jsx
@@ -1,26 +1,28 @@
 "use client";
 
 import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ReturnHandler() {
   const searchParams = useSearchParams();
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const vnp_ResponseCode = searchParams.get("vnp_ResponseCode");
 
     if (vnp_ResponseCode) {
+      setLoading(false);
       if (vnp_ResponseCode === "00") {
         toast.success("Thanh toán thành công!");
         router.push("/order-placed");
       } else {
-        toast.error(`Thanh toán thất bại: Mã ${vnp_ResponseCode}`);
+        toast.error(`Thanh toán thất bại: ${vnp_ResponseCode}`);
         router.push("/cart");
       }
     }
-  }, [searchParams, router]);
+  }, [searchParams]);
 
-  return null; // Không cần render gì cả
+  return <div>{loading ? "Đang xử lý..." : "Kết quả thanh toán"}</div>;
 }
