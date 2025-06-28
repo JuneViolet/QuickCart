f408c0efc245994dbe4b195ca718343be4411b5d
tieuhythang <tieuhythang2000@gmail.com>
2025-06-28 21:05:51 +0700

    save

--- models/Order.js
+++ models/Order.js
@@ -36,54 +36,6 @@
 // }
 
 // export default Order;
-// //models/Order.js
-// import mongoose from "mongoose";
-// import Address from "./Address";
-
-// const orderSchema = new mongoose.Schema({
-//   userId: {
-//     type: String, // Thay ObjectId bằng String
-//     required: true,
-//     ref: "User", // Vẫn giữ ref để populate nếu cần
-//   },
-//   items: [
-//     {
-//       product: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         required: true,
-//         ref: "Product",
-//       },
-//       variantId: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: "Variant",
-//         required: true,
-//       },
-//       quantity: { type: Number, required: true },
-//     },
-//   ],
-//   amount: { type: Number, required: true },
-//   address: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Address",
-//     required: true,
-//   },
-//   trackingCode: { type: String, required: true },
-//   status: {
-//     type: String,
-//     enum: ["pending", "confirmed", "shipped", "delivered", "canceled"],
-//     default: "pending",
-//   },
-//   date: { type: Date, default: Date.now },
-// });
-
-// let Order;
-// try {
-//   Order = mongoose.model("Order");
-// } catch {
-//   Order = mongoose.model("Order", orderSchema);
-// }
-
-// export default Order;
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
